a385008799891f32a1101e831fbffc687d98587c
Omkar Deshmukh <omdesh.0407@gmail.com>
2021-09-05 23:07:29 +0530

    Test

--- CommandsService/Controllers/PlatformsController.cs
+++ CommandsService/Controllers/PlatformsController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CommandsService.Controllers
 {
-    //
     [Route("api/c/[controller]")]
     [ApiController]
     public class PlatformsController: ControllerBase
